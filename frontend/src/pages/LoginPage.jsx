c707638cc22df19396ca902fb184d6d523156476
Valentin Mendez <vmendez@comunidad.unnoba.edu.ar>
2026-06-02 19:43:54 -0300

    feat: crear estructura inicial de pagina login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,22 @@
 function LoginPage() {
-  return (
-	 <div>LoginPage</div>
-  )
+	return (
+		<div className="bg-[#F7F9FB] h-screen w-screen">
+			<div className="flex flex-col w-full h-full justify-center items-center">
+				<div>
+					<div>
+						<img src="/logo-unnoba.png" alt="logo-unnoba" className="rounded-xl w-30" />
+					</div>
+					<div>
+						<h1>Sistema de Tutorías</h1>
+						<h2>Universidad Nacional del Noroeste de la Provincia de Buenos Aires</h2>
+					</div>
+				</div>
+				<div>
+					<h1>recuadro</h1>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default LoginPage
+export default LoginPage;
