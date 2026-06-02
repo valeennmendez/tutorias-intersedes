c51d9b413e74c55998769072ca25f764f5c8e4d4
Valentin Mendez <vmendez@comunidad.unnoba.edu.ar>
2026-06-02 19:37:53 -0300

    feat: crear ruta de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import PruebaPage from "./pages/PruebaPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/prueba" element={<PruebaPage />} />
+			<Route path="/login" element={<LoginPage />} />
 		</Routes>
 	);
 }
