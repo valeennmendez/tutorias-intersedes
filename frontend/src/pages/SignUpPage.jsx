fdeb371af9d82820f9394721e6879b951694c1ba
Valentin Mendez <vmendez@comunidad.unnoba.edu.ar>
2026-06-02 22:57:33 -0300

    style: cambios en estilos de titulos y subtitulos de registro

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -7,8 +7,14 @@ function SignUpPage() {
 						<img src="/logo-unnoba.png" alt="logo-unnoba" className="rounded-xl w-30" />
 					</div>
 					<div className="text-center">
-						<h1 className="font-bold text-2xl">Sistema de Tutorías</h1>
-						<h2 className="font-normal text-slate-600 text-sm">Universidad Nacional del Noroeste de la Provincia de Buenos Aires</h2>
+						<h1 className="font-bold text-2xl">Crear Cuenta</h1>
+						<h2 className="font-normal text-slate-600 text-sm">Sistema de Tutorías UNNOBA</h2>
+					</div>
+				</div>
+				<div className="bg-white rounded-lg shadow-md w-120 min-h-80 py-5">
+					<div className="flex flex-col items-center">
+						<h1 className="font-semibold text-xl">Registro</h1>
+						<h2 className="text-slate-600 font-medium text-sm">Completa tus datos para crear tu cuenta</h2>
 					</div>
 				</div>
 			</div>
