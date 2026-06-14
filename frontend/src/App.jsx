a4ea81bb1c5692a24841c079ecec166103dabfe3
Valentin Mendez <vmendez@comunidad.unnoba.edu.ar>
2026-06-14 04:22:21 -0300

    feat: conexion con backend en formulario registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,21 @@ import PruebaPage from "./pages/PruebaPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import PostulacionTutorClient from "./pages/PostulacionTutor/PostulacionTutorClient";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return (
-    <div className="light bg-white text-black min-h-screen" style={{ colorScheme: 'light' }}>
-      <Routes>
-        <Route path="/prueba" element={<PruebaPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/postulacion-tutor" element={<PostulacionTutorClient />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="light bg-white text-black min-h-screen" style={{ colorScheme: "light" }}>
+			<Routes>
+				<Route path="/prueba" element={<PruebaPage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/postulacion-tutor" element={<PostulacionTutorClient />} />
+			</Routes>
+
+			<Toaster />
+		</div>
+	);
 }
 
 export default App;
-
--- frontend/src/store/auth.store.js
+++ frontend/src/store/auth.store.js
@@ -1,13 +1,27 @@
 import { create } from "zustand";
 import { axiosInstance } from "../utils/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create(() => ({
 	registrarUsuario: async (data) => {
 		try {
 			const res = await axiosInstance.post("/auth/register", data);
 			console.log(res);
+			toast.success(res.data);
 		} catch (error) {
-			console.log("Error al registrar usuario: ", error);
+			const backendError = error.response?.data;
+
+			if (typeof backendError === "string") {
+				// Caso simple: mensaje directo
+				toast.error(backendError);
+			} else if (typeof backendError === "object") {
+				// Caso múltiple: recorrer las claves y mostrar cada error
+				Object.values(backendError).forEach((msg) => {
+					toast.error(msg);
+				});
+			} else {
+				toast.error("Error desconocido al registrar usuario");
+			}
 		}
 	},
 }));
