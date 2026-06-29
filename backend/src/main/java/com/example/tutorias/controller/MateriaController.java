94a4277a1a61be565ddfdaf985d96c0e07f97f2e
RingoI <mgibarzabal@comunidad.unnoba.edu.ar>
2026-06-29 16:52:46 -0300

    correccion de errores

--- backend/src/main/java/com/example/tutorias/controller/MateriaController.java
+++ backend/src/main/java/com/example/tutorias/controller/MateriaController.java
@@ -1,9 +1,15 @@
 package com.example.tutorias.controller;
 
+import com.example.tutorias.dto.materia.CreateMateriaRequest;
 import com.example.tutorias.dto.materia.MateriaResponse;
 import com.example.tutorias.service.MateriaService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +33,20 @@ public class MateriaController {
     @GetMapping
     public List<MateriaResponse> obtenerTodasMaterias() {
         return materiaService.obtenerTodasMaterias();
+    }
+
+    @PostMapping
+    public MateriaResponse crearMateria(@RequestBody CreateMateriaRequest request) {
+        return materiaService.crearMateria(request.getNombre());
+    }
+
+    @PutMapping("/{id}")
+    public MateriaResponse actualizarMateria(@PathVariable Long id, @RequestBody CreateMateriaRequest request) {
+        return materiaService.actualizarMateria(id, request.getNombre());
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarMateria(@PathVariable Long id) {
+        materiaService.eliminarMateria(id);
     }
 }
