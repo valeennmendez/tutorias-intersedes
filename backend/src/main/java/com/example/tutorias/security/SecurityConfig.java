7f061bab74f499e76af2451cedb4e9fd176b73c5
Valentin Mendez <vmendez@comunidad.unnoba.edu.ar>
2026-07-03 14:58:18 -0300

    feat: se agrega el endpont /carrera como publica

--- backend/src/main/java/com/example/tutorias/security/SecurityConfig.java
+++ backend/src/main/java/com/example/tutorias/security/SecurityConfig.java
@@ -115,6 +115,7 @@ public class SecurityConfig {
                 .requestMatchers("/tutores/**").hasAnyRole("TUTOR", "ADMIN")
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers("/feedback/**").authenticated()
+                .requestMatchers("/carreras/**").permitAll()
 
                 .anyRequest().authenticated()
             )
