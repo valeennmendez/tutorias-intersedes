33a474a1b7aa0fb9be464354635a49833ca1b113
Valentin Mendez <vmendez@comunidad.unnoba.edu.ar>
2026-07-03 14:31:35 -0300

    feat: creacion del carreraService con el nombramiento de la funcion obtenerCarreras

---
+++ backend/src/main/java/com/example/tutorias/service/CarreraService.java
@@ -0,0 +1,9 @@
+package com.example.tutorias.service;
+
+import java.util.List;
+
+import com.example.tutorias.entity.Carrera;
+
+public interface CarreraService {
+    List<Carrera> obtenerCarreras();
+}
